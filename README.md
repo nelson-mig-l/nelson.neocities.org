eff2d13e7fc52fa527069c355fd8fe8cbee8af06
nelson-mig-l <nelson.mig.l@gmail.com>
2018-10-06 01:09:03 +0200

    improved deploy script

---
+++ README.md
@@ -0,0 +1,4 @@
+# What is this?
+My personal website. Kind of a "business card".
+
+
--- deploy.sh
+++ deploy.sh
@@ -2,28 +2,36 @@
 
 apikey=`cat api.key`
 
-echo "========================================================================="
-echo "= Deploy nelson.neocities.org                                           ="
-echo "=-----------------------------------------------------------------------="
-echo "= IMPORTANT - PLEASE READ                                               ="
-echo "= Currently this script does not remove deleted files from the server.  ="
-echo "= You will have to remove them manually.                                ="
-echo "= If you deleted files please remove then manually from the server.     ="
-echo "= Now press any key to start uploading files.                           ="
-echo "=-----------------------------------------------------------------------="
-echo "=                                      $apikey ="
-echo "========================================================================="
-#read -p "Press any key to continue... " -n1 -s
+echo ".=======================================================================."
+echo "| Deploy nelson.neocities.org                                           |"
+echo "|-----------------------------------------------------------------------|"
+echo "| IMPORTANT - PLEASE READ                                               |"
+echo "| Currently this script does not remove deleted files from the server.  |"
+echo "| You will have to remove them manually.                                |"
+echo "| If you deleted files please remove then manually from the server.     |"
+echo "| Now press any key to start uploading files.                           |"
+echo "|-----------------------------------------------------------------------|"
+echo "|                                      $apikey |"
+echo "'======================================================================='"
 read -n1 -s
 
 
 for toupload in `ls site`; do
     echo "Uploading $toupload ..."
-    curl \
-    -H "Authorization: Bearer $apikey" \
-    -F "$toupload=$toupload" \
-    https://neocities.org/api/upload
-    echo "    ... done!"
+    RESPONSE=`curl -sS\
+        -H "Authorization: Bearer $apikey" \
+        -F "$toupload=@site/$toupload" \
+        https://neocities.org/api/upload`
+    RESULT=`echo $RESPONSE | jq -r '.result'`
+    if [ "$RESULT" != "success" ];
+    then
+        echo "FAILED TO UPLOAD $toupload - RESULT $RESULT"
+        echo "RESPONSE WAS:"
+        echo "$RESPONSE"
+        break
+    else
+        echo "    ... done!"
+    fi
 done
-read -p "Press any key to continue... " -n1 -s
-echo .
+
+echo "Terminated."
